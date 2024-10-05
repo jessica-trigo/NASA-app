6cda0762701b29b294075c25e971322af3d9c987
Jéssica Trigo <jessica.trigo05@icloud.com>
2024-10-05 18:34:05 +0200

    Initial Commit

---
+++ NASA app/NASA_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_appApp.swift
+//  NASA app
+//
+//  Created by Jéssica Trigo on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NASA_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
