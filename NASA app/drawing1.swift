6c14a2681a876b91c2bea002c8584977713d9bb9
Jéssica Trigo <jessica.trigo05@icloud.com>
2024-10-06 19:58:36 +0200

    drawing + other details

---
+++ NASA app/drawing1.swift
@@ -0,0 +1,18 @@
+//
+//  drawing1.swift
+//  NASA app
+//
+//  Created by Jéssica Trigo on 06/10/24.
+//
+
+import SwiftUI
+
+struct drawing1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    drawing1()
+}
