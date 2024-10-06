2dec518d14f793069db27ac39c6d36b2178fc04c
Jéssica Trigo <jessica.trigo05@icloud.com>
2024-10-06 22:45:23 +0200

    quuiz 4

---
+++ NASA app/q4.swift
@@ -0,0 +1,18 @@
+//
+//  q4.swift
+//  NASA app
+//
+//  Created by Jéssica Trigo on 06/10/24.
+//
+
+import SwiftUI
+
+struct q4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    q4()
+}
